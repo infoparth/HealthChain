9068e9e9395f314f699d8fdd3a025a2dc84613e0
infoparth <parthatharv2606@gmail.com>
2023-10-21 23:54:35 +0530

    Initial commit, pushing the final code

--- src/landingpage.jsx
+++ src/landingpage.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import logo from './image/securemed.png';
-
+import logo from "./image/securemed.png";
 
 export const Securemed = () => {
-
   //contract_address = 0x10E169701b033fE4F78C55c79C6519AeB4bB4909
 
   const navigate = useNavigate();
 
-  
-    return (
-      <div  className = "Firstpage">
-        <div><h2 >Welcome To Securemed!</h2><br/></div>
-        <div><img className = "image" src={logo} alt="Logo"></img></div>
-        <h4>A Blockchain-Enabled Medical Records Manager with AI-Powered Insights and NFT Monetization</h4>
-        <button onClick={() => navigate('/login')}>Click to Login</button>
-        </div>
-    )
-  
-}
+  return (
+    <div className="Firstpage">
+      <div>
+        <h2>Welcome To Securemed!</h2>
+        <br />
+      </div>
+      <div>
+        <img className="image" src={logo} alt="Logo"></img>
+      </div>
+      <h4>
+        A Blockchain-Enabled Medical Records Manager with AI-Powered Insights
+        and NFT Monetization
+      </h4>
+      <button className="loginButton" onClick={() => navigate("/login")}>
+        Click to Login
+      </button>
+    </div>
+  );
+};
 
-export default Securemed
+export default Securemed;
--- src/patient.jsx
+++ src/patient.jsx
@@ -1,24 +1,34 @@
-import React  from 'react';
-import './Navbar.css';
-import Navbar from './Navbar';
-import { useNavigate } from 'react-router-dom';
-  
-function Patient () {
+import React from "react";
+import "./Navbar.css";
+import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
+function Patient() {
   const navigate = useNavigate();
 
   return (
-    <div className='theFirstPage'><Navbar/>
-    <div className = "Firstpage">What would you like to do?
-      <div><button onClick={() => navigate('/upload')}> Upload Documents </button>{'      '}
-      <button onClick={() => navigate('/view')}> View Documents </button><br/>
-      <button onClick={() => navigate('/access')}> Access Management </button>{'    '}
-      <button onClick={() => navigate('/nft')}>NFT Monetization</button><br/>
-      <button> Get lifestyle recommendations </button></div>
+    <div className="theFirstPage">
+      <Navbar />
+      <div className="Firstpage">
+        What would you like to do?
+        <div>
+          <button onClick={() => navigate("/upload")}>
+            {" "}
+            Upload Documents{" "}
+          </button>
+          {"      "}
+          <button onClick={() => navigate("/view")}> View Documents </button>
+          <br />
+          <button onClick={() => navigate("/access")}>
+            {" "}
+            Access Management{" "}
+          </button>
+          {"    "}
+          <button onClick={() => navigate("/nft")}>NFT Monetization</button>
+          <br />
+        </div>
+      </div>
     </div>
-    </div>
-    
-
-  )
+  );
 }
-export default Patient
+export default Patient;
